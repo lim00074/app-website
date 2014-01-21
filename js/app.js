52e635057bc2b0ae76f5a641e295db000a5c3501
lim00074 <jillianlim@Jillians-MacBook-Pro-2.local>
2014-01-21 16:17:26 -0500

    Carousel

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//EXPANDABLE MENU
+
 var navBtn = document.querySelector('.nav-btn');
 var navTop = document.querySelector('.nav-top');
 
@@ -11,3 +13,45 @@ navBtn.addEventListener('click', function (eventObject) {
 		navTop.setAttribute('data-state', 'open');
 	}
 }, false);
+
+//CAROUSEL
+var $items = $('.carousel-items img');
+var switchItems = function (outgoing, incoming) {
+	$items.eq(incoming).attr('data-state', 'incoming').fadeIn(250, function () {
+		$items.eq(outgoing).attr('data-state', '').hide();
+		$items.eq(incoming).attr('data-state', 'current');
+	});
+	//.eq specifies a specific number
+};
+
+$items.filter(':not([data-state="current"])').hide();
+	//This hides the images that are not being shown
+
+
+//NEXT BUTTON:
+$('.next').on('click', function () {
+	var current = $items.filter('[data-state="current"]').index();
+	//.index allows jquery to keep a count
+	var next = current + 1;
+
+	if (next >= $items.length) {
+		next = 0;
+		//This will loop the carousel back to image 0
+	}
+
+	switchItems(current, next);
+});
+
+//PREVIOUS BUTTON:
+$('.previous').on('click', function () {
+	var current = $items.filter('[data-state="current"]').index();
+	//.index allows jquery to keep a count
+	var previous = current - 1;
+
+	if (previous < 0) {
+		previous = $items.length - 1;
+	}
+	//Don't actually need to have an if statement when going backwards to the end
+
+	switchItems(current, previous);
+});
