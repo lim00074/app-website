c4218864f942094864998f2fbf417f1153b675ab
lim00074 <jillianlim@Jillians-MacBook-Pro-2.local>
2014-01-30 03:36:30 -0500

    Added video

--- js/app.js
+++ js/app.js
@@ -14,6 +14,27 @@ navBtn.addEventListener('click', function (eventObject) {
 	}
 }, false);
 
+//VIDEO
+var $dialog = $('#video');
+var $placeholder = $('#video-placeholder');
+
+dialogPolyfill.registerDialog($dialog.get(0));
+
+$('.placeholder').on('click', function () {
+	$placeholder.html('<iframe src="http://player.vimeo.com/video/81441143?color=c9ff23&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+	//.get() returns the native JS element for us to access
+	//When using jQuery we don't have access to the original HTML elements directly; we are using jQuery to manipulate them
+	$dialog.get(0).showModal();
+	//.show() allows multiple dialogs
+	//.showModal() single dialog, with no user interaction behind (darkens background)
+});
+
+$('#btn-close').on('click', function () {
+	$dialog.get(0).close();
+	$placeholder.html('');
+});
+
+
 //CAROUSEL
 var $items = $('.carousel-items img');
 var switchItems = function (outgoing, incoming) {
