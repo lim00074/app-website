44f972bfca7ae16da834aae5cc1ded9402e40326
lim00074 <jillianlim@Jillians-MacBook-Pro-2.local>
2014-02-06 10:16:02 -0500

    AJAX random testimonials

--- js/app.js
+++ js/app.js
@@ -84,3 +84,14 @@ var $plane = $('.plane');
 $win.on('scroll', function () {
 	$plane.css('transform', 'rotate('+ $win.scrollTop()/5 +'deg)');
 });
+
+//RANDOM TESTIMONIALS
+var rand = Math.round(Math.random() * 2); 
+	//Math.round rounds the number up, so that it's no longer decimal numbers
+	//Multiply by 2 so that the number can go up to 2 (if not, it's just decimal numbers ≤ 1)
+var file = 'testimonial-' + rand +'.html';
+	//This generates the random file name
+
+//console.log(file); This will test it out
+
+$('.rand-test').load(file);
